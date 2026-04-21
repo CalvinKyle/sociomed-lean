6427cd91836fb7896253a799d7524010641b45ed
CalvinKyle <50634070+CalvinKyle@users.noreply.github.com>
2026-04-21 16:56:37 +0300

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 # Import your models and config
 from app.core.config import DATABASE_URL
-from app.schemas.db import Base 
+from app.models.db import Base 
 
 # This is the Alembic Config object
 config = context.config
